4c7fb3395718fe14e8587748f7e742fab8f491b0
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-05-28 22:04:04 -0300

    implement a runner for illustrative purposes

--- src/main/java/com/carlosoliveira/runnerz/RunnerzApplication.java
+++ src/main/java/com/carlosoliveira/runnerz/RunnerzApplication.java
@@ -1,13 +1,40 @@
 package com.carlosoliveira.runnerz;
 
+import com.carlosoliveira.runnerz.run.Location;
+import com.carlosoliveira.runnerz.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class RunnerzApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RunnerzApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            Run run = new Run(
+                    1,
+                    "First run",
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusHours(1),
+                    5,
+                    Location.OUTDOOR
+            );
+            log.info("Run: {}", run);
+        };
+
+    }
 }
